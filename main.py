47824b91a20994e615d9d0bc281fefd37937672a
Trevonte Wigfall <29114309+creationsoftre@users.noreply.github.com>
2022-04-29 06:59:19 +0000

    Code formatted

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ ec2_tag_value = "ACServer01"
 message = ""
 messages = [None]
 
+
 ### bot events ###
 @bot.event
 async def on_ready():
@@ -55,12 +56,14 @@ async def on_ready():
 
 ### bot commands ###
 
+
 # Logging - send embed to aws-logs channel when command is executed
 @bot.event
 async def on_application_command_completion(ctx):
     channel = bot.get_channel(log_channel_id)
     embed = discord.Embed(
-        description=f"Executed Command: {ctx.command}\nAuthor: {ctx.author.mention}\nLocation: {ctx.channel.mention}",
+        description=
+        f"Executed Command: {ctx.command}\nAuthor: {ctx.author.mention}\nLocation: {ctx.channel.mention}",
         timestamp=datetime.now(),
         color=discord.Colour.green(),
     )
@@ -74,7 +77,8 @@ async def on_application_command_completion(ctx):
 async def on_application_command(ctx):
     channel = bot.get_channel(log_channel_id)
     embed = discord.Embed(
-        description=f"Invoked Command: {ctx.command}\nAuthor: {ctx.author.mention}\nLocation: {ctx.channel.mention}",
+        description=
+        f"Invoked Command: {ctx.command}\nAuthor: {ctx.author.mention}\nLocation: {ctx.channel.mention}",
         timestamp=datetime.now(),
         color=discord.Colour.yellow(),
     )
@@ -92,8 +96,8 @@ async def on_application_command_error(ctx, error):
         )
     elif isinstance(error, commands.MissingRole):
         await ctx.respond(
-            "You do not have the role required to run this command.", ephemeral=True
-        )
+            "You do not have the role required to run this command.",
+            ephemeral=True)
     elif isinstance(error, commands.CommandNotFound):
         pass
     else:
@@ -124,7 +128,8 @@ async def ping(ctx):
 
 
 ## Start EC2 command
-@bot.slash_command(guild_ids=[server_id], description="command to start ec2 instance")
+@bot.slash_command(guild_ids=[server_id],
+                   description="command to start ec2 instance")
 @commands.has_role(ec2_role_id)
 @commands.cooldown(1, 60, commands.BucketType.user)
 async def ec2_start(ctx):
@@ -133,8 +138,10 @@ async def ec2_start(ctx):
 
 ## Stop EC2 command
 
+
 ## EC2 Stop command
-@bot.slash_command(guild_ids=[server_id], description="command to stop ec2 instance")
+@bot.slash_command(guild_ids=[server_id],
+                   description="command to stop ec2 instance")
 @commands.has_role(ec2_role_id)
 @commands.cooldown(1, 60, commands.BucketType.user)
 async def ec2_stop(ctx):
@@ -143,8 +150,10 @@ async def ec2_stop(ctx):
 
 ## Reboot EC2 command
 
+
 ## EC2 Reboot Command
-@bot.slash_command(guild_ids=[server_id], description="command to reboot ec2 instance")
+@bot.slash_command(guild_ids=[server_id],
+                   description="command to reboot ec2 instance")
 @commands.has_role(ec2_role_id)
 @commands.cooldown(1, 60, commands.BucketType.user)
 async def ec2_reboot(ctx):
@@ -153,36 +162,41 @@ async def ec2_reboot(ctx):
 
 ## Get EC2 status command
 
+
 ## EC2 Status Command
-@bot.slash_command(
-    guild_ids=[server_id], description="command to to get ec2 instance status"
-)
+@bot.slash_command(guild_ids=[server_id],
+                   description="command to to get ec2 instance status")
 @commands.has_role(ec2_role_id)
 @commands.cooldown(1, 15, commands.BucketType.user)
 async def ec2_status(ctx):
-    await functions.send_instance_state(
-        ctx, ec2_client, ec2_tag_value, instance_launchtime, instance_az, instance_name
-    )
+    await functions.send_instance_state(ctx, ec2_client, ec2_tag_value,
+                                        instance_launchtime, instance_az,
+                                        instance_name)
 
 
 # error handling on commands
 
 # command error handling
 
+
 ##Guide
 # Help Guide command
-@bot.slash_command(guild_ids=[server_id], description="A Guide on how to use the bot.")
+@bot.slash_command(guild_ids=[server_id],
+                   description="A Guide on how to use the bot.")
 async def guide(ctx):
     embed = discord.Embed(
         title="AWS Admin BOT Guide",
-        description="This AWS bot is to allow you to start ,stop, and reboot a AWS EC2 Instance.",
-        color=discord.Colour.blue(),  # Pycord provides a class with default colors you can choose from
+        description=
+        "This AWS bot is to allow you to start ,stop, and reboot a AWS EC2 Instance.",
+        color=discord.Colour.blue(
+        ),  # Pycord provides a class with default colors you can choose from
     )
 
     embed.set_author(
         name="creationsoftre",
         url="https://github.com/creationsoftre/",
-        icon_url="https://cdn.discordapp.com/attachments/950967021938556959/966750552757272646/cbktre_logo.png",
+        icon_url=
+        "https://cdn.discordapp.com/attachments/950967021938556959/966750552757272646/cbktre_logo.png",
     )
     embed.add_field(
         name="Commands",
@@ -191,45 +205,45 @@ async def guide(ctx):
     )
     embed.add_field(name="Start EC2 Instance", value="/ec2_start", inline=True)
     embed.add_field(name="Stop EC2 Instance", value="/ec2_stop", inline=True)
-    embed.add_field(name="Reboot EC2 Instance", value="/ec2_reboot", inline=True)
-    embed.add_field(name="EC2 Instance Status", value="/ec2_status", inline=True)
+    embed.add_field(name="Reboot EC2 Instance",
+                    value="/ec2_reboot",
+                    inline=True)
+    embed.add_field(name="EC2 Instance Status",
+                    value="/ec2_status",
+                    inline=True)
     embed.set_footer(text="© 2022, creationsoftre")
 
     view = functions.View_UI(ctx)
-    await ctx.respond(
-        "Hello! See what commands are used in this bot.", embed=embed, view=view
-    )
+    await ctx.respond("Hello! See what commands are used in this bot.",
+                      embed=embed,
+                      view=view)
+
+
 #Clear command Choices
-log_choice=[
-        OptionChoice(name="Yes",value="Yes"),
-        OptionChoice(name="No",value="No")
+log_choice = [
+    OptionChoice(name="Yes", value="Yes"),
+    OptionChoice(name="No", value="No")
 ]
 
+
 # Clear Command
-@bot.slash_command(guild_ids=[server_id], description="clear messages in chat.")
+@bot.slash_command(guild_ids=[server_id],
+                   description="clear messages in chat.")
 @commands.has_role(administrator)
-async def clear(
-  ctx,
-  amount: Option(
+async def clear(ctx, amount: Option(
     discord.SlashCommandOptionType.integer,
     description="amount of messages",
     required=True,
-  ),
-  
-  user: Option(
+), user: Option(
     discord.SlashCommandOptionType.user,
     description="purge messages from user",
     required=False,
     default=None,
-  ),
-  logging: Option(
-    discord.SlashCommandOptionType.string,
-    description="enable or disable logging",
-    required=False,
-    default=None,
-    choices=log_choice
-  )
-):
+), logging: Option(discord.SlashCommandOptionType.string,
+                   description="enable or disable logging",
+                   required=False,
+                   default=None,
+                   choices=log_choice)):
     await ctx.response.defer(ephemeral=True)
     channel = bot.get_channel(log_channel_id)
     if ctx.channel.id == log_channel_id and logging == "Yes":
@@ -247,10 +261,10 @@ async def clear(
         # purge without logging in other channels
         await functions.purge(ctx, amount, user)
 
+
 # Package Command
-@bot.slash_command(
-    guild_ids=[server_id], description="package logs before todays date."
-)
+@bot.slash_command(guild_ids=[server_id],
+                   description="package logs before todays date.")
 @commands.has_role(administrator)
 async def package(
     ctx,
@@ -268,8 +282,8 @@ async def package(
         await functions.package_logging(ctx, channel, amount, messages)
     else:
         await ctx.respond(
-            f"/package command can only be ran in {channel.mention}", ephermal=True
-        )
+            f"/package command can only be ran in {channel.mention}",
+            ephermal=True)
 
 
 bot.run(discord_token)
